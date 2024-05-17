8ea33ca9c85b9aa71d71a9143bf0f3bda01eda3f
LordBao666 <916900021@qq.com>
2024-05-17 17:44:20 +0800

    Version 1 is Finished
    when a square of 16x16 grid is being mouseover,
    it will be colored by black.

---
+++ JavaScriptBasics/etch-a-sketch/sketch.js
@@ -0,0 +1,51 @@
+const container = document.querySelector(".container");
+const containerWidth = container.clientWidth;
+console.log(containerWidth)
+//the defaut number of squares in each side. 
+//e.g  16x16 implies the squareNumEachSide is  16
+const squareNumEachSide = 16; 
+
+let currentColor = "black"
+//the variable indicates if the sub elements of container  is being clicked.
+let isContainerClicked = false;
+let lastMouseDownElement = document.body
+
+
+document.body.addEventListener("mousedown",(event)=>{
+  lastMouseDownElement = event.target;
+  //if one of container's subelements is being mousedown
+  if (lastMouseDownElement.parentNode === container) {
+    lastMouseDownElement.style.backgroundColor = currentColor;
+    isContainerClicked = true;
+  }
+});
+
+document.body.addEventListener("mouseup", (event) => {
+    isContainerClicked = false;
+});
+
+
+container.addEventListener("mouseover", (event) => {
+  //if one of container's subelements is being mouseover
+  if(event.target.parentNode===container){
+    if(isContainerClicked===true){
+      event.target.style.backgroundColor=currentColor;
+    }
+  }
+});
+
+
+
+
+function createSquares(_squareNumEachSide=squareNumEachSide){
+    const squareWidth=containerWidth / _squareNumEachSide;//the width of each square
+    console.log(squareWidth)
+    for(let i=0;i<_squareNumEachSide * _squareNumEachSide;i++){
+      const div = document.createElement("div");
+      div.style.width = squareWidth + "px"; //Note it is  squareWidth+"px" instead of squareWidth.
+      div.classList.add("defaultGrid");
+      container.appendChild(div);
+    }
+}
+
+createSquares(squareNumEachSide)
